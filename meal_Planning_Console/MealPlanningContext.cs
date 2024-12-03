dc35a9924280f3986da780ecf0ba92bda399ca21
Mike Halsey <mhalsey0@gmail.com>
2024-12-02 21:24:10 -0500

    updated DbContext to use SQLite

---
+++ meal_Planning_Console/MealPlanningContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using System.IO;
+
+namespace meal_Planning_Console
+{
+    public class MealPlanningContext : DbContext
+    {
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<GroceryList> GroceryLists { get; set;}
+
+        public string DbPath { get; }
+
+        public MealPlanningContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "meal_planning.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
